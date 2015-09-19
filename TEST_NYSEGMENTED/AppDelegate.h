c9500e1faa86688e99f340a8e6603ac2c21451a6
Colas <colasjojo@yahoo.fr>
2015-09-19 10:32:00 -0400

    Initial Commit

---
+++ TEST_NYSEGMENTED/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TEST_NYSEGMENTED
+//
+//  Created by Colas on 19/09/2015.
+//  Copyright (c) 2015 colas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TEST_NYSEGMENTED/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEST_NYSEGMENTED
+//
+//  Created by Colas on 19/09/2015.
+//  Copyright (c) 2015 colas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
